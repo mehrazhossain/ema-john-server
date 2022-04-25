f102c335e65998db24761dc13b906a054748c49d
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-25 12:15:17 +0600

    connect mongodb

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0eew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// API
+app.get('/', (req, res) => {
+  res.send('Welcome to Ema-John Server Side!!');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
